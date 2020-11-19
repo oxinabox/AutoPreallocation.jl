0495f671017df79691e24de65dbee1260079a186
marius <mariusmillea@gmail.com>
2020-11-19 03:13:31 -0800

    improve tests

--- test/inference.jl
+++ test/inference.jl
@@ -1,46 +1,59 @@
-using AutoPreallocation
+using AutoPreallocation, Test
 
 function inferred_and_nonallocating(f)
     pf = preallocate(f)[2]
-    @test (@inferred(pf()); true)
-    @test @allocated(pf())==0
+    @inferred(pf())
+    @allocated(pf()) == 0
 end
 
 # the different forms of Array constructors which exist
-inferred_and_nonallocating() do
+@test inferred_and_nonallocating() do
+    Array{Float64}(undef, 4)
+end
+@test inferred_and_nonallocating() do
+    Array{Float64,1}(undef, 4)
+end
+@test inferred_and_nonallocating() do
     Array{Float64}(undef, 4, 5)
 end
-inferred_and_nonallocating() do
+@test inferred_and_nonallocating() do
     Array{Float64}(undef, (4, 5))
 end
-inferred_and_nonallocating() do
+@test inferred_and_nonallocating() do
     Array{Float64,2}(undef, 4, 5)
 end
-inferred_and_nonallocating() do
+@test inferred_and_nonallocating() do
     Array{Float64,2}(undef, (4, 5))
 end
-inferred_and_nonallocating() do
+@test inferred_and_nonallocating() do
     Array{Float64,2}(undef, (4, 5))
 end
-inferred_and_nonallocating() do
+@test inferred_and_nonallocating() do
     Array{Float64,3}(undef, (4, 5, 6))
 end
 
 # slightly deeper
-inferred_and_nonallocating() do 
+@test inferred_and_nonallocating() do 
     zeros(Float64,3)
 end
 
 # two different typed arrays in the record
-inferred_and_nonallocating() do 
+@test inferred_and_nonallocating() do 
     zeros(Float32,3)
     zeros(Float64,3)
 end
 
 # some more complex broadcasting and temporary arrays
-inferred_and_nonallocating() do 
+@test inferred_and_nonallocating() do 
     x = zeros(Float32,3)
     y = zeros(Float64,3)
     z = 2 .* x .+ y
     3 .+ z
 end
+
+
+# issue #24
+struct Foo end
+Foo() = Vector{Float64}(undef,10)
+@test_broken inferred_and_nonallocating(Foo) # dont understand why this isnt inferred
+@test inferred_and_nonallocating(()->Foo())
